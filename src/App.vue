85d5983ee88148fafbf0c29a78cd62c59cdb02ee
eden <singlesaul@163.com>
2020-01-02 22:20:22 +0800

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,40 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="father">
+      <span>父亲有钱：<span class="data">{{father}}</span></span>
+    </div>
+    <Childone :child="father" v-on:update:child="father=$event"/>
+    <Childtwo />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Childone from "./components/childone"
+import Childtwo from "./components/childtwo"
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      father:100
+    }
+  },
+  components:{
+    Childone,
+    Childtwo
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.father{
+  padding: 20px;
+  margin: 20px;
+  border: 1px solid black;
+}
+button{
+  margin: 0 20px;
+}
+span.data{
+  color: brown;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+let eventHub = new Vue()
+Vue.prototype.$eventHub = eventHub
+
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
